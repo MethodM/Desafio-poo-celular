47836f5f2673c4f1815f509f8d207c0a5627ae12
MethodM <casagrande.theus@gmail.com>
2025-06-12 00:56:24 -0300

    Aprimorando classe Iphone, retirando conteúdo de código redundante.

--- src/main/java/iphone/desafio/Iphone.java
+++ src/main/java/iphone/desafio/Iphone.java
@@ -21,87 +21,25 @@ public class Iphone {
         return modelo;
     }
 
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
-    }
 
     public String getMarca() {
         return marca;
     }
 
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
 
     public String getSistemaOperacional() {
         return sistemaOperacional;
-    }
-
-    public void setSistemaOperacional(String sistemaOperacional) {
-        this.sistemaOperacional = sistemaOperacional;
     }
 
     public AparelhoTelefonicoImpl getAparelhoTelefonico() {
         return aparelhoTelefonico;
     }
 
-    public void setAparelhoTelefonico(AparelhoTelefonicoImpl aparelhoTelefonico) {
-        this.aparelhoTelefonico = aparelhoTelefonico;
-    }
-
     public NavegadorInternetImpl getNavegadorInternet() {
         return navegadorInternet;
     }
 
-    public void setNavegadorInternet(NavegadorInternetImpl navegadorInternet) {
-        this.navegadorInternet = navegadorInternet;
-    }
-
-    public NavegadorInternetImpl getAdicionarNovaAba() {
-        return navegadorInternet;
-    }
-
-    public void setAdicionarNovaAba(NavegadorInternetImpl navegadorInternet) {
-        this.navegadorInternet = navegadorInternet;
-    }
-
-    public NavegadorInternetImpl getAtualizarPagina() {
-        return navegadorInternet;
-    }
-
-    public void setAtualizarPagina(NavegadorInternetImpl navegadorInternet) {
-        this.navegadorInternet = navegadorInternet;
-    }
-
     public ReprodutorMusicalImpl getReprodutorMusical() {
         return reprodutorMusical;
-    }
-
-    public void setReprodutorMusical(ReprodutorMusicalImpl reprodutorMusical) {
-        this.reprodutorMusical = reprodutorMusical;
-    }
-
-    public ReprodutorMusicalImpl getTocar() {
-        return reprodutorMusical;
-    }
-
-    public void setTocar(ReprodutorMusicalImpl reprodutorMusical) {
-        this.reprodutorMusical = reprodutorMusical;
-    }
-
-    public ReprodutorMusicalImpl getPausar() {
-        return reprodutorMusical;
-    }
-
-    public void setPausar(ReprodutorMusicalImpl pausar) {
-        this.reprodutorMusical = pausar;
-    }
-
-    public ReprodutorMusicalImpl getSelecionarMusica() {
-        return reprodutorMusical;
-    }
-
-    public void setSelecionarMusica(ReprodutorMusicalImpl selecionarMusica) {
-        this.reprodutorMusical = selecionarMusica;
     }
 }
