a64148b48b7733f05ee8bf625bdeb548a62516a1
MethodM <casagrande.theus@gmail.com>
2025-06-12 00:57:02 -0300

    Aprimorando classe ReprodutorMusicalImpl, introduzindo mensagens nos métodos da classe.

--- src/main/java/iphone/desafio/ReprodutorMusicalImpl.java
+++ src/main/java/iphone/desafio/ReprodutorMusicalImpl.java
@@ -3,16 +3,19 @@ package iphone.desafio;
 public class ReprodutorMusicalImpl implements ReprodutorMusical {
     @Override
     public void tocar() {
+        System.out.println("Tocando música.");
 
     }
 
     @Override
     public void pausar() {
+        System.out.println("Pausando música.");
 
     }
 
     @Override
     public void selecionarMusica(String musica) {
+        System.out.println("Selecionando Música: " + musica);
 
     }
 }
