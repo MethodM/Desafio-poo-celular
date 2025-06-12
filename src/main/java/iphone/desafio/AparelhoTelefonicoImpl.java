c64005c21b23aed8903871e721ff974cd31ae542
MethodM <casagrande.theus@gmail.com>
2025-06-12 00:55:51 -0300

    Aprimorando classe AparelhoTelefonicoImpl, incluindo mensagens nos métodos.

--- src/main/java/iphone/desafio/AparelhoTelefonicoImpl.java
+++ src/main/java/iphone/desafio/AparelhoTelefonicoImpl.java
@@ -4,16 +4,18 @@ public class AparelhoTelefonicoImpl implements AparelhoTelefonico {
 
     @Override
     public void ligar(String numero) {
-
+        System.out.println("Fazendo chamada.");
     }
 
     @Override
     public void atender() {
+        System.out.println("Atendendo chamada, chamada em andamento.");
 
     }
 
     @Override
     public void iniciarCorreioVoz() {
+        System.out.println("Deixe sua mensagem, logo retornaremos a ligação.");
 
     }
 }
